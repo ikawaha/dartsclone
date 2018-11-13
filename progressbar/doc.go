7189c655d03ca87e8e7771b59bc73702648edff4
ikawaha <ikawaha@users.noreply.github.com>
2018-11-13 17:44:33 +0900

    Fix

--- progressbar/doc.go
+++ progressbar/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package progress bar is a sample implementation of ProgressFunction interface.
+// Package progressbar is a sample implementation of ProgressFunction interface.
 package progressbar
